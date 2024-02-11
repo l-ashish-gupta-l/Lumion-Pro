13fe4b09b3a4c0b66a1ad971e79b5d040be52197
l-ashish-gupta-l <ashish3072002@gmail.com>
2024-02-11 11:11:25 +0530

    Mobile menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import Landingpage from './components/Landingpage'
 import Marquee from './components/Marquee'
@@ -9,11 +9,24 @@ import Cards from './components/Cards'
 import LocomotiveScroll from 'locomotive-scroll'
 import Getreadyeye from './components/Getreadyeye'
 import Footer from './components/Footer'
+import MobileMenu from './components/MobileMenu'
 function App() {
   const locomotiveScroll = new LocomotiveScroll();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleNavbarBtnClick = () => {
+    setMenuOpen(!menuOpen);
+    
+  };
+
+  const handleMobileMenuBtnClick = () => {
+    setMenuOpen(false);
+
+  }
   return (
     <div className="w-full min-h-screen text-[#212121] bg-[#F1F1F1]">
-      <Navbar />
+      <Navbar onNavbarBtnClick={handleNavbarBtnClick} />
+      <MobileMenu isOpen={menuOpen} onMobileMenuBtnClick={handleMobileMenuBtnClick} />
       <Landingpage />
       <Marquee />
       <About />
