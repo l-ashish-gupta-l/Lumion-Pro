e9896100c7903ac1f35c8978a7167e517498f161
l-ashish-gupta-l <ashish3072002@gmail.com>
2024-02-11 17:55:49 +0530

    DONE

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -2,15 +2,6 @@ import React, { useEffect, useRef, useState } from 'react'
 import video from "../assest/Main-HQ-1.mp4"
 function Eyes() {
     const [rotate, setrotate] = useState(0)
-    const video_div = useRef(null);
-
-    const videodiv = () => {
-        if (video_div.current) {
-            console.log(video_div.current, 'clicked');
-            video_div.current.style.opacity = '100%';
-            video_div.current.style.transition = 'opacity 1s ease-in-out';
-        }
-    };
 
     useEffect(() => {
         window.addEventListener("mousemove", (e) => {
@@ -26,14 +17,43 @@ function Eyes() {
         })
     }, [])
 
+    const div_ref = useRef(null);
+
+
+    const [X, setX] = useState("0");
+    const [Y, setY] = useState("0");
+    useEffect(() => {
+
+        div_ref.current.addEventListener("mousemove", (e) => {
+            setX(e.clientX - 100);
+            setY(e.clientY - 100);
+        })
+
+    }, [])
+
+    const [vissible, setvissible] = useState("hidden")
+    const videoRef = useRef(null);
+
+    const togglePauseAndVisibility = () => {
+        const video = videoRef.current;
+
+        if (video.paused) {
+            video.play();
+            setvissible("visible");
+        } else {
+            video.pause();
+            setvissible("hidden");
+        }
+    };
+
 
 
     return (
         <div className='relative w-full h-[50vh] lg:h-screen overflow-hidden'>
             <div data-scroll data-scroll-speed="-.7" className='h-[50vh] lg:h-full lg:w-full flex justify-center items-center bg-cover bg-center bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg")] relative'>
-                <div className='absolute flex gap-10 lg:gap-20 w-1/2 h-1/3 lg:w-[50vw] lg:h-[40vh]  lg:px-20 py-5   '>
+                <div className='absolute flex gap-10 lg:gap-20 w-1/2 h-1/3 lg:w-[50vw] lg:h-[40vh]  lg:px-20 py-5    '>
                     <div className='w-full rounded-full  bg-white flex justify-center items-center'>
-                        <div onClick={videodiv} className=' w-[15vw] h-[15vw] lg:w-[10vw] lg:h-[10vw] relative flex justify-center items-center bg-zinc-900 rounded-full  font-[Neue] text-white lg:text-sm text-[.5em]  '>
+                        <div onClick={togglePauseAndVisibility} className=' w-[15vw] h-[15vw] lg:w-[10vw] lg:h-[10vw] relative flex justify-center items-center bg-zinc-900 rounded-full  font-[Neue] text-white lg:text-sm text-[.5em]   '>
                             PLAY
                             <div style={{ transform: `translate(-50% ,-50%) rotate(${rotate}deg)` }} className='line absolute p-1 line top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-5'>
                                 <div className='w-3 h-3  lg:w-5 lg:h-5  bg-zinc-300 rounded-full'></div>
@@ -50,12 +70,15 @@ function Eyes() {
                     </div>
                 </div>
             </div>
-            <div ref={video_div} className='absolute z-10 w-full h-full top-0 left-0 bg-red-300 overflow-hidden opacity-0 hidden'>
-                <div className='w-full h-full relative'>
-                    <video src={video} autoPlay loop muted />
+
+            <div ref={div_ref} className={`absolute left-0 top-0 w-full h-full  ${vissible} `}>
+                <div className="relative w-full h-full overflow-hidden bg-red-500">
+                    <button onClick={togglePauseAndVisibility} className={`absolute z-30 top-[85%] left-[80%] px-2 py-1 bg-black font-[Neue] rounded-2xl text-white`}>
+                        PAUSE
+                    </button>
+                    <video ref={videoRef} src={video} className="z-10 object-cover w-full h-full" autoPlay={true} loop muted />
                 </div>
             </div>
-
 
         </div>
     )
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,74 +1,84 @@
 import React, { useState } from 'react'
 import Card1 from "../assest/card1.png"
+import Card2 from "../assest/vise.jpg"
+import Card3 from "../assest/Card 3.jpg"
+import Card4 from "../assest/Card4.png"
 import { motion } from "framer-motion"
 
 
-function Features() {
+function FeatureCard({ Img1, Img2, Name1, Name2 }) {
     const [ishovery1, setishovery1] = useState(false)
     const [ishovery2, setishovery2] = useState(false)
     return (
-        <div className='w-full lg:py-20 '>
-            <div className='w-full px-5 lg:px-16 pb-10 border-b-[1.5px] border-zinc-300'>
-                <h1 className='text-3xl lg:text-6xl font-[Neue]'>Featured projects</h1>
+        <div className=' relative cards w-full h-full lg:flex lg:gap-5 mt-5 '>
+            <div className='lg:w-1/2  lg:flex lg:flex-col'>
+                <div className='px-4 py-2 flex justify-start items-center '>
+                    <div className='w-3 h-3 rounded-full bg-black'></div>
+                    <h1 className='ml-2 font-[Neue] text-sm'>FYDE</h1>
+                </div>
+                <div onMouseEnter={() => setishovery1(true)} onMouseLeave={() => setishovery1(false)} className=' cardbox w-full  flex items-center justify-center '>
+
+                    <h1 className='absolute text-[#CDEA68] hidden z-10 lg:flex text-9xl ml-[50%] font-[Founders] overflow-hidden'>
+                        {"FYDE".split("").map((item, index) =>
+                            <motion.span initial={{ y: "100%" }} animate={ishovery1 ? ({ y: "0" }) : ({ y: "100%" })}
+                                transition={{ ease: [0.22, 1, 0.36, 1], delay: index * .06 }} key={index} className='inline-block '>{item}</motion.span>)}
+                    </h1>
+
+                    <motion.div initial={{ scale: "1" }} animate={ishovery1 ? ({ scale: ".9" }) : ({ scale: "1" })}
+                        transition={{ ease: 'easeInOut', duration: "5" }} className='card w-full h-full rounded-xl overflow-hidden  transition-transform '  >
+                        <img src={Img1} alt="card1" className='object-cover' />
+
+                    </motion.div>
+                </div>
+                <div className='p-2 flex '>
+                    {["AUDIT", "COPYWRITING", "SALES DECK"].map((item, index) => {
+                        return <h1 className='ml-2 px-3 py-1   rounded-3xl font-[Neue] text-sm border-black border-[1.5px]'>{item}</h1>
+                    })}
+                </div>
             </div>
-            <div className='lg:px-16 px-5'>
-                <div className=' relative cards w-full h-full lg:flex lg:gap-5 mt-5   '>
-                    <div className='lg:w-1/2  lg:flex lg:flex-col     '>
-                        <div className='px-4 py-2 flex justify-start items-center   '>
-                            <div className='w-3 h-3 rounded-full bg-black'></div>
-                            <h1 className='ml-2 font-[Neue] text-sm'>FYDE</h1>
-                        </div>
-                        <div onMouseEnter={() => setishovery1(true)} onMouseLeave={() => setishovery1(false)} className=' cardbox w-full  flex items-center justify-center '>
+            <div className='lg:w-1/2  lg:flex lg:flex-col     '>
+                <div className='px-4 py-2 flex justify-start items-center   '>
+                    <div className='w-3 h-3 rounded-full bg-black'></div>
+                    <h1 className='ml-2 font-[Neue] text-sm'>VISE</h1>
+                </div>
+                <div onMouseEnter={() => setishovery2(true)} onMouseLeave={() => setishovery2(false)} className=' cardbox w-full  flex items-center justify-center '>
 
-                            <h1 className='absolute text-[#CDEA68] hidden z-10 lg:flex text-9xl ml-[50%] font-[Founders] overflow-hidden'>
-                                {"FYDE".split("").map((item, index) =>
-                                    <motion.span initial={{ y: "100%" }} animate={ishovery1 ? ({ y: "0" }) : ({ y: "100%" })}
-                                        transition={{ ease: [0.22, 1, 0.36, 1], delay: index * .06 }} key={index} className='inline-block '>{item}</motion.span>)}
-                            </h1>
+                    <h1 className='absolute text-[#CDEA68] hidden z-10 lg:flex text-9xl mr-[50%] font-[Founders] overflow-hidden'>
+                        {"VISE".split("").map((item, index) =>
+                            <motion.span initial={{ y: "100%" }} animate={ishovery2 ? ({ y: "0" }) : ({ y: "100%" })}
+                                transition={{ ease: [0.22, 1, 0.36, 1], delay: index * .06 }} key={index} className='inline-block '>{item}</motion.span>)}
+                    </h1>
 
-                            <motion.div initial={{ scale: "1" }} animate={ishovery1 ? ({ scale: ".9" }) : ({ scale: "1" })}
-                                transition={{ ease: 'easeInOut', duration: "5" }} className='card w-full h-full rounded-xl overflow-hidden  transition-transform '  >
-                                <img src={Card1} alt="card1" className='object-cover' />
+                    <motion.div initial={{ scale: "1" }} animate={ishovery2 ? ({ scale: ".9" }) : ({ scale: "1" })}
+                        transition={{ ease: 'easeInOut', duration: "5" }} className='card w-full h-full rounded-xl overflow-hidden  transition-transform '  >
+                        <img src={Img2} alt="card2" className='object-cover' />
 
-                            </motion.div>
-                        </div>
-                        <div className='p-2 flex  '>
-                            <h1 className='ml-2 px-3 py-1   rounded-3xl font-[Neue] text-sm border-black border-[1.5px]'>AUDIT</h1>
-                            <h1 className='ml-2 px-3 py-1   rounded-3xl font-[Neue] text-sm border-black border-[1.5px]'>AUDIT</h1>
-                            <h1 className='ml-2 px-3 py-1   rounded-3xl font-[Neue] text-sm border-black border-[1.5px]'>AUDIT</h1>
-                        </div>
-                    </div>
-                    <div className='lg:w-1/2  lg:flex lg:flex-col     '>
-                        <div className='px-4 py-2 flex justify-start items-center   '>
-                            <div className='w-3 h-3 rounded-full bg-black'></div>
-                            <h1 className='ml-2 font-[Neue] text-sm'>FYDE</h1>
-                        </div>
-                        <div onMouseEnter={() => setishovery2(true)} onMouseLeave={() => setishovery2(false)} className=' cardbox w-full  flex items-center justify-center '>
-
-                            <h1 className='absolute text-[#CDEA68] hidden z-10 lg:flex text-9xl mr-[50%] font-[Founders] overflow-hidden'>
-                                {"FYDE".split("").map((item, index) =>
-                                    <motion.span initial={{ y: "100%" }} animate={ishovery2 ? ({ y: "0" }) : ({ y: "100%" })}
-                                        transition={{ ease: [0.22, 1, 0.36, 1], delay: index * .06 }} key={index} className='inline-block '>{item}</motion.span>)}
-                            </h1>
-
-                            <motion.div initial={{ scale: "1" }} animate={ishovery2 ? ({ scale: ".9" }) : ({ scale: "1" })}
-                                transition={{ ease: 'easeInOut', duration: "5" }} className='card w-full h-full rounded-xl overflow-hidden  transition-transform '  >
-                                <img src={Card1} alt="card1" className='object-cover' />
-
-                            </motion.div>
-                        </div>
-                        <div className='p-2 flex  '>
-                            <h1 className='ml-2 px-3 py-1   rounded-3xl font-[Neue] text-sm border-black border-[1.5px]'>AUDIT</h1>
-                            <h1 className='ml-2 px-3 py-1   rounded-3xl font-[Neue] text-sm border-black border-[1.5px]'>AUDIT</h1>
-                            <h1 className='ml-2 px-3 py-1   rounded-3xl font-[Neue] text-sm border-black border-[1.5px]'>AUDIT</h1>
-                        </div>
-                    </div>
-
-
+                    </motion.div>
+                </div>
+                <div className='p-2 flex '>
+                    {["AGENCY", "COMPONY PRESENTATION"].map((item, index) => {
+                        return <h1 className='ml-2 px-3 py-1   rounded-3xl font-[Neue] text-sm border-black border-[1.5px]'>{item}</h1>
+                    })}
                 </div>
             </div>
         </div>
     )
 }
+function Features() {
+
+    return (
+        <div className='w-full lg:py-20 '>
+            < div className='w-full px-5 lg:px-16 pb-10 border-b-[1.5px] border-zinc-300' >
+                <h1 className='text-3xl lg:text-6xl font-[Neue]'>Featured projects</h1>
+            </ div>
+            <div className='lg:px-16 px-5'>
+                <FeatureCard Img1={Card1} Img2={Card2} Name1={"FYDE"} />
+                <FeatureCard Img1={Card3} Img2={Card4} />
+            </div>
+
+        </div >
+    )
+}
 
 export default Features
+export { FeatureCard }
